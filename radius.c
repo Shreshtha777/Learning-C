49540657dd6a69cb835cc9f7109be8904090fb71
Shreshtha <indoriyashreshtha@gmail.com>
2022-08-01 10:41:26 -0700

    Add new program

---
+++ radius.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float shreshtha;
+    printf("enter radius of a circle");
+    scanf("%f",&shreshtha);
+    printf("area is:%f", 3.14*shreshtha*shreshtha);
+    return 0;
+
+}
