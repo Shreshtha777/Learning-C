891b10a3997798a9425a1e9a5191e2d964dbafbf
Shreshtha <indoriyashreshtha@gmail.com>
2022-08-09 10:10:13 -0700

    initial commit

---
+++ average.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    int c;
+    int avg;
+    printf("enter a value number abc:");
+    scanf("%d%d%d",&a,&b,&c);
+    avg =(a+b+c)/3;
+    printf("average of a three number%d",avg);
+    return 0;
+
+
+
+
+}
